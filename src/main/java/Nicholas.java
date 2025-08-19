c58d3e26d11a27f79fcedfd73b0add14b391381b
Nic_soh_lame <sohnicholas02@gmail.com>
2025-08-19 17:14:57 +0800

    Added user input and echo feature

--- src/main/java/Nicholas.java
+++ src/main/java/Nicholas.java
@@ -1,13 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner; //Import the Scanner Class
+
 public class Nicholas {
-    public static String line = "--------------------";
+
+    public static final String line = "--------------------";
+    public static final Scanner scanner = new Scanner(System.in); // Creating new Scanner Object
+    public static final String exitCommand = "bye";
+    public static final String NAME = "Nicholas";
+
     public static void main(String[] args) {
         System.out.println(line);
         greet();
-        exit();
+        echo();
     }
 
     public static void greet() {
-        System.out.println("Hello! I'm Nicholas");
+        System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
         System.out.println(line);
     }
@@ -15,5 +23,20 @@ public class Nicholas {
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
+    }
+
+    public static void echo() {
+        while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println(line);
+
+            if (Objects.equals(userInput, exitCommand)) {
+                exit();
+                break;
+            }
+
+            System.out.println(userInput);
+            System.out.println(line);
+        }
     }
 }
