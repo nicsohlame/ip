ff97872fc7d98e53aaa5f4396195a51091865b48
Nic_soh_lame <sohnicholas02@gmail.com>
2025-08-19 16:30:47 +0800

    Exit method

--- src/main/java/Nicholas.java
+++ src/main/java/Nicholas.java
@@ -3,11 +3,17 @@ public class Nicholas {
     public static void main(String[] args) {
         System.out.println(line);
         greet();
+        exit();
     }
 
     public static void greet() {
         System.out.println("Hello! I'm Nicholas");
         System.out.println("What can I do for you?");
+        System.out.println(line);
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
 }
