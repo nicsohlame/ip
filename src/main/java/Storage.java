3b982533aebc47652c9631b0883ab818fe85b52d
Nic_soh_lame <sohnicholas02@gmail.com>
2025-08-29 17:06:27 +0800

    Add storage class for file handling

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.IOException;
 
+/**
+ * Handles file storage and writing
+ */
 public class Storage {
+
+    /* Default file path */
+    private static final String STORAGE_PATH = "./tasks/Nicholas.txt";
+
+    /* Handles folder and file creation */
+    public void fileSetup() {
+        File file = new File(STORAGE_PATH);
+
+        if (file.getParentFile() != null && !file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
+
